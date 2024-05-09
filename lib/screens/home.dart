2aa0b59ed07edc7cc166e9b3863de7b74a236455
Rafael Domingues <rafael.domingues@confirm8.com>
2024-05-09 14:20:14 -0300

    feat: add appBar and basic body with snack bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,8 +8,61 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final List _toDoList = [];
+  final TextEditingController _toDoController = TextEditingController();
+
+  void onPressed(BuildContext context) {
+    if (_toDoController.text.isEmpty) {
+      SnackBar alert = SnackBar(
+        content: const Text('Nome da tarefa é obrigatório'),
+        duration: const Duration(seconds: 5),
+        action: SnackBarAction(
+            label: 'ok',
+            onPressed: () {
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+            }),
+      );
+
+      ScaffoldMessenger.of(context).removeCurrentSnackBar();
+      ScaffoldMessenger.of(context).showSnackBar(alert);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ToDo List'),
+        centerTitle: true,
+      ),
+      body: Builder(
+        builder: (context) => Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                    controller: _toDoController,
+                    maxLength: 50,
+                    decoration:
+                        const InputDecoration(label: Text('Nova tarefa')),
+                  )),
+                  SizedBox(
+                    height: 45.0,
+                    width: 45.0,
+                    child: FloatingActionButton(
+                      onPressed: () => onPressed(context),
+                      child: const Icon(Icons.save),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
